be9e0d23fe386b3a533cd056e493ed4e429e3aaa
kirill_tarasov <kirilltaras1792@yandex.ru>
2024-02-24 19:59:26 +0700

    Создать класс для работы с базой данных

---
+++ src/database.py
@@ -0,0 +1,15 @@
+class DBVacancies:
+    def __init__(self):
+        pass
+
+    def create_database(self):
+        """Создание базы данных."""
+        pass
+
+    def create_tables(self):
+        """Создание таблиц в созданной базе данных."""
+        pass
+
+    def insert_data(self):
+        """Заполнение таблиц данными."""
+
