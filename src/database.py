05febbbed174273c1e55ad0f7751fc028d086133
kirill_tarasov <kirilltaras1792@yandex.ru>
2024-03-17 13:55:52 +0700

    Создать метод заполнения таблиц

--- src/database.py
+++ src/database.py
@@ -50,4 +50,25 @@ class DBVacancies:
         except psycopg2.ProgrammingError as e:
             print(f'{e}:Не удалось создать таблицы')
 
+    def insert_data(self):
+        """Заполнение таблиц."""
+        conn_params = psycopg2.connect(
+            database=self.database_name,
+            user=self.user_name,
+            host=self.host
+        )
+        try:
+            with psycopg2.connect(**conn_params) as conn:
+                with conn.cursor() as cur:
+                    for vacancy in self.dict_vacancies:
+                        # Вставляем данные о компании, если ее еще нет в таблице
+                        cur.execute("INSERT INTO employers (employer_id, employer_name) "
+                                    "VALUES (%s, %s) ON CONFLICT (employer_id) DO NOTHING",
+                                    (vacancy["employer_id"], vacancy["employer_name"]))
 
+                        # Вставляем данные о вакансии
+                        cur.execute("INSERT INTO vacancies (vacancy_name, salary, employer_id) VALUES (%s, %s, %s)",
+                                    (vacancy["vacancy_name"], vacancy["salary"], vacancy["employer_id"]))
+                conn.commit()
+        except psycopg2.ProgrammingError as e:
+            print(f'{e}:Не удалось добавить данные')
