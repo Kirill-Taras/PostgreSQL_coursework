20009922f09285fbd52ed8f5a1973d5c32c63c07
kirill_tarasov <kirilltaras1792@yandex.ru>
2024-03-17 13:54:56 +0700

    Создать метод создания таблиц

--- src/database.py
+++ src/database.py
@@ -2,10 +2,11 @@ import psycopg2
 
 
 class DBVacancies:
-    def __init__(self, database_name, user_name, host):
+    def __init__(self, database_name, user_name, host, dict_vacancies):
         self.database_name = database_name
         self.user_name = user_name
         self.host = host
+        self.dict_vacancies = dict_vacancies
 
     def create_database(self):
         """Создание базы данных."""
@@ -19,34 +20,34 @@ class DBVacancies:
                     cur.execute(f'DROP DATABASE IF EXISTS {self.database_name}')
                     cur.execute(f'CREATE DATABASE {self.database_name}')
         except psycopg2.ProgrammingError as e:
-            print('Отсутствует подключение')
+            print(f'{e}:Отсутствует подключение')
 
     def create_tables(self):
-        """Создание таблиц в созданной базе данных."""
-        conn = psycopg2.connect(
+        """Создание таблиц"""
+        conn_params = psycopg2.connect(
             database=self.database_name,
             user=self.user_name,
             host=self.host
         )
-        with conn.cursor() as cur:
-            cur.execute("""
-                    CREATE TABLE IF NOT EXISTS employers (
-                    employer_id SERIAL PRIMARY KEY,
-                    employer_name VARCHAR(255) UNIQUE
-                 )
-                    """)
-            cur.execute("""
-                    CREATE TABLE IF NOT EXISTS vacancies (
-                        vacancy_id SERIAL PRIMARY KEY,
-                        vacancy_name TEXT NOT NULL,
-                        salary INT,
-                        employer_id INT REFERENCES employers(employer_id)
-                    )
-                    """)
-        conn.commit()
-        conn.close()
+        try:
+            with psycopg2.connect(**conn_params) as conn:
+                with conn.cursor() as cur:
+                    cur.execute("""
+                                        CREATE TABLE IF NOT EXISTS employers (
+                                        employer_id SERIAL PRIMARY KEY,
+                                        employer_name VARCHAR(255) UNIQUE
+                                     )
+                                        """)
+                    cur.execute("""
+                                        CREATE TABLE IF NOT EXISTS vacancies (
+                                            vacancy_id SERIAL PRIMARY KEY,
+                                            vacancy_name TEXT NOT NULL,
+                                            salary INT,
+                                            employer_id INT REFERENCES employers(employer_id)
+                                        )
+                                        """)
+                    conn.commit()
+        except psycopg2.ProgrammingError as e:
+            print(f'{e}:Не удалось создать таблицы')
 
-
-def insert_data(self):
-        """Заполнение таблиц данными."""
 
